d584292c482e26c15bc41983671b1057126fdaaa
Wendel Campos Aguiar <79800240+wendelcampos@users.noreply.github.com>
2025-01-30 13:48:34 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import { globalStyles } from "@/styles/global";
+import type { AppProps } from "next/app";
+
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from "@/styles/pages/app";
+
+import Image from "next/image";
+
+globalStyles()
+
+export default function App({ Component, pageProps }: AppProps) {
+
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />;
+    </Container>
+  )
+}
+
