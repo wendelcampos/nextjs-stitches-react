1911d8d7bbd3e307f8a6b144d29350e935dc54ba
Wendel Campos Aguiar <79800240+wendelcampos@users.noreply.github.com>
2025-02-03 15:56:34 -0300

    add features

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+
+  images: {
+    domains: [
+      'files.stripe.com',
+    ],
+  }
 };
 
 export default nextConfig;
--- src/pages/product/[id]/index.tsx
+++ src/pages/product/[id]/index.tsx
@@ -1,10 +1,82 @@
-import { useRouter } from 'next/router'
+import { stripe } from '@/lib/stripe';
+import { ImageContainer, ProductContainer, ProductDetails } from '@/styles/pages/products';
+import { GetStaticProps } from 'next';
+import Image from 'next/image';
+import { Stripe } from 'stripe';
 
-export default function Product() {
-    const { query } = useRouter();
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+    defaultPriceId: string;
+  }
+}
 
+export default function Product({ product }: ProductProps) {
+    function handleBuyProduct() {
+      console.log(product.defaultPriceId)
+    }
+    
     return (
-      <h1>Product: {JSON.stringify(query)}</h1>
+      <ProductContainer>
+        <ImageContainer>
+          <Image src={ product.imageUrl } width={520} height={480} alt=''/>
+        </ImageContainer>
+        <ProductDetails>
+          <h1>{ product.name }</h1>
+          <span>{ product.price }</span>
+
+          <p>{ product.description }</p>
+
+          <button onClick={handleBuyProduct}>
+            Comprar agora
+          </button>
+        </ProductDetails>
+      </ProductContainer>
     );
   }
+
+  export const getStaticPaths = async () => {
+    return {
+      // ou coloca nada ou colocar entre os 5 e 10 produtos mais acessados
+      paths: [
+        { params: { id: 'prod_RgPIQDfhQT67T3' } },
+      ],
+      fallback: true,
+    }
+  }
   
+  export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+    if (!params) {
+      return {
+        notFound: true,
+      };
+    }
+
+    const productId = params.id;
+
+    const product = await stripe.products.retrieve(productId, {
+      expand: ['default_price'],
+    });
+
+    const price = product.default_price as Stripe.Price
+
+    return {
+      props: {
+        product: {      
+          id: product.id,
+          name: product.name,
+          imageUrl: product.images[0],
+          price: new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format((price.unit_amount ?? 0) / 100)},
+          description: product.description,
+          defaultPriceId: price.id,
+      },
+      revalidate: 60 * 60 * 1, // 1 hour
+    };
+  }
